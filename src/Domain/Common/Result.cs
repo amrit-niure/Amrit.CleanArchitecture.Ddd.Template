a20d20212e2b9e70262e6fa3115d0257c788a410
Amrit Niure <niureamrit2@gmail.com>
2026-08-08 20:20:35 +0930

    Refactor Result classes and add TodoItem entity with creation logic

--- src/Domain/Common/Result.cs
+++ src/Domain/Common/Result.cs
@@ -28,19 +28,4 @@ namespace Domain.Common
             value is not null ? Success(value) : Failure<TValue>(Error.NullValue);
     }
 
-    public class Result<TValue> : Result
-    {
-        private readonly TValue? _value;
-
-        protected internal Result(TValue? value, bool isSuccess, Error error) : base(isSuccess, error)
-        {
-            _value = value;
-        }
-
-        public TValue Value => IsSuccess
-            ? _value!
-            : throw new InvalidOperationException("The value of a failure result cannot be accessed.");
-
-        public static implicit operator Result<TValue>(TValue? value) => Create(value);
-    }
 }
---
+++ src/Domain/TodoItem.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Domain.Common;
+
+namespace Blank.Domain;
+
+internal class TodoItem : Entity
+{
+    private TodoItem(Guid id, string title) : base(id)
+    {
+        Title = title;
+    }
+
+    public string Title { get; private set; }
+
+    public static Result<TodoItem> Create(string title)
+    {
+        if (string.IsNullOrWhiteSpace(title)) return Result.Failure<TodoItem>("Title is required");
+        new TodoItem(title);
+    }
+}

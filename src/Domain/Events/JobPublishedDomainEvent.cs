ca41d80577246a7d19644bd7f3e76828e9f5e74c
Amrit Niure <niureamrit2@gmail.com>
2026-08-08 21:31:52 +0930

    Remove JobPublishedDomainEvent and SalaryRange classes from the domain layer

--- src/Domain/Events/JobPublishedDomainEvent.cs
+++
@@ -1,6 +0,0 @@
-using Domain.Common;
-
-namespace Domain.Events
-{
-    public sealed record JobPublishedDomainEvent(Guid JobId) : IDomainEvent;
-}
--- src/Domain/TodoItem.cs
+++ src/Domain/TodoItem.cs
@@ -1,5 +1,9 @@
-﻿
-public class TodoItem
+﻿public class TodoItem
 {
+    public string Title { get; set; }
 
+    public TodoItem(string title)
+    {
+        Title = title;
+    }
 }
